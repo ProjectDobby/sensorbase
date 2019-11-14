eb6ccd853509971554dde08b896f17b35b61c120
Cedric <50961672+CCedDev@users.noreply.github.com>
2019-11-14 15:27:58 +0100

    + WiFi Connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,23 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include "WebSocketClient.h"
+#include <ESP8266WiFi.h>
 
 #define Sensor_Input 5
 #define RST_PIN D3
 #define SS_PIN D8
 
-int Sensor_Type = 1;
+int Sensor_Type = 0;
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;
+String printHex(byte *buffer, byte bufferSize);
 // Type : 0 Movement Sensor | Type: 1 RFID Sensor 
-
-
-
-
 
 void Movement_Sensor();
 void RFID_Sensor();
-String printHex(byte *buffer, byte bufferSize);
+void Connect_to_Wlan();
 
 void setup() {
   Serial.begin(115200);
@@ -26,14 +25,13 @@ void setup() {
 
   SPI.begin();
   rfid.PCD_Init();
-  // TODO : FIND API WITH WEBSERVER 
 
-  // TODO : CONNECT TO WEBSOCKET API
   Serial.print("calibrating Device ");
   for(int i = 0; i < 10; i++){
     Serial.print(".");
     delay(1000);
   }
+  Connect_to_Wlan();
 }
 
 void loop() {
@@ -87,5 +85,12 @@ String printHex(byte *buffer, byte bufferSize) {
   return id;
 }
 
-
-
+void Connect_to_Wlan(){
+  WiFi.begin("4 Euro pro MB","Welches Passwort?");
+  Serial.print("Connecting to WiFi");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.print("Connectet!");
+} 
